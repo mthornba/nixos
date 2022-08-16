e63c53804fd3b8dccdaaefead1f7f39ef9f2297e
mthornba <matt.thornback@gmail.com>
2022-08-16 13:10:02 -0400

    UPD: Enable backlight h/w keys

--- configuration.nix
+++ configuration.nix
@@ -150,6 +150,15 @@
 
   programs.zsh.enable = true;
 
+  programs.light.enable = true;
+  services.actkbd = {
+    enable = true;
+    bindings = [
+      { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
+      { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+    ];
+  };
+
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
