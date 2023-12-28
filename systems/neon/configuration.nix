0cbe86e9b26b960d5635eb23c2ac0aad0a02f75c
Matt Thornback <matt.thornback@gmail.com>
2023-12-28 16:53:56 -0500

    feat: add dnsutils & openssl to system pkgs

--- systems/neon/configuration.nix
+++ systems/neon/configuration.nix
@@ -174,8 +174,10 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     curl
+    dnsutils
     git
     nfs-utils
+    openssl
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
   ];
