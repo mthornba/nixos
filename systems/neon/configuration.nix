0da5b6e3dfc3e414112964241e2d7cd2eff4ff0a
Matt Thornback <matt.thornback@gmail.com>
2024-04-21 13:28:11 -0400

    feat: enable sshd

--- systems/neon/configuration.nix
+++ systems/neon/configuration.nix
@@ -227,7 +227,12 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+    };
+  };
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 32500 ]; #Plexamp remote control
