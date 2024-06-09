b01cd4e849aa9136f45d928c01fa7e9f7b717599
Matt Thornback <matt.thornback@gmail.com>
2024-06-09 12:54:40 -0400

    feat: enable i3

--- systems/neon/configuration.nix
+++ systems/neon/configuration.nix
@@ -96,9 +96,21 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.defaultSession = "gnome-xorg";
+  services.xserver = {
+    displayManager.gdm.enable = false;
+    desktopManager.gnome.enable = false;
+    displayManager.defaultSession = "none+i3";
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        i3status # gives you the default i3 status bar
+        i3lock #default i3 screen locker
+        i3blocks #if you are planning on using i3blocks over i3status
+      ];
+    };
+  };
 
   # Configure keymap in X11
   services.xserver = {
