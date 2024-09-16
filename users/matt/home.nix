38a7e845712583f4bdfe31f5237ec27d8ffa9580
Matt Thornback <matt.thornback@palitronica.com>
2024-09-16 08:39:42 -0400

    feat: add newline to starship prompt

--- users/matt/home.nix
+++ users/matt/home.nix
@@ -202,7 +202,7 @@ in
       enable = true;
       # custom settings
       settings = {
-        add_newline = false;
+        add_newline = true;
         aws.disabled = true;
         gcloud.disabled = true;
         line_break.disabled = true;
