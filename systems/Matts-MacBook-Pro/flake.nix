9796764fe5eb63a1c3c04299b8449a547a5ea68f
Matt Thornback <matt.thornback@palitronica.com>
2025-01-15 13:40:11 -0500

    feat: enable brew services

--- systems/Matts-MacBook-Pro/flake.nix
+++ systems/Matts-MacBook-Pro/flake.nix
@@ -22,9 +22,13 @@
       url = "github:homebrew/homebrew-cask";
       flake = false;
     };
+    homebrew-services = {
+      url = "github:homebrew/homebrew-services";
+      flake = false;
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nix-homebrew, homebrew-bundle, homebrew-core, homebrew-cask, home-manager, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nix-homebrew, homebrew-bundle, homebrew-core, homebrew-cask, homebrew-services, home-manager, nixpkgs }:
   let
     user = "matt";
     configuration = { pkgs, ... }: {
@@ -126,6 +130,7 @@
               "homebrew/homebrew-core" = homebrew-core;
               "homebrew/homebrew-cask" = homebrew-cask;
               "homebrew/homebrew-bundle" = homebrew-bundle;
+              "homebrew/homebrew-services" = homebrew-services;
             };
 
             # Optional: Enable fully-declarative tap management
