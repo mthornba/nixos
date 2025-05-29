4867d59926ff51ebbdba7ffbca77a76375ff2ee4
Matt Thornback <matt.thornback@palitronica.com>
2025-05-28 20:35:24 -0400

    fix(vim): powerline symbols in airline
    https://github.com/vim-airline/vim-airline/wiki/Dummies-Guide-to-the-status-bar-symbols-(Powerline-fonts)-on-Fedora,-Ubuntu-and-Windows

--- users/matt/home.nix
+++ users/matt/home.nix
@@ -295,7 +295,47 @@ in
           '';
         }
         vim-airline
-        vim-airline-themes
+        { plugin = vim-airline-themes;
+          config = ''
+            let g:airline_theme='solarized'
+            let g:airline_solarized_bg='dark'
+            let g:airline_powerline_fonts = 1
+
+            if !exists('g:airline_symbols')
+              let g:airline_symbols = {}
+            endif
+
+            " unicode symbols
+            let g:airline_left_sep = '»'
+            let g:airline_left_sep = '▶'
+            let g:airline_right_sep = '«'
+            let g:airline_right_sep = '◀'
+            let g:airline_symbols.crypt = '🔒'
+            let g:airline_symbols.linenr = '☰'
+            let g:airline_symbols.linenr = '␊'
+            let g:airline_symbols.linenr = '␤'
+            let g:airline_symbols.linenr = '¶'
+            let g:airline_symbols.maxlinenr = ' '
+            let g:airline_symbols.maxlinenr = '㏑'
+            let g:airline_symbols.branch = '⎇'
+            let g:airline_symbols.paste = 'ρ'
+            let g:airline_symbols.paste = 'Þ'
+            let g:airline_symbols.paste = '∥'
+            let g:airline_symbols.spell = 'Ꞩ'
+            let g:airline_symbols.notexists = 'Ɇ'
+            let g:airline_symbols.whitespace = 'Ξ'
+
+            " powerline symbols
+            let g:airline_left_sep = ''
+            let g:airline_left_alt_sep = ''
+            let g:airline_right_sep = ''
+            let g:airline_right_alt_sep = ''
+            let g:airline_symbols.branch = ''
+            let g:airline_symbols.readonly = ''
+            let g:airline_symbols.linenr = '☰'
+            let g:airline_symbols.maxlinenr = ''
+          '';
+        }
         vim-colors-solarized
         vim-colorschemes
         vim-fugitive
