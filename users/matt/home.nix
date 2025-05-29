ea4b957ce40401bf98270340f5c364701a42e389
Matt Thornback <matt.thornback@palitronica.com>
2025-05-28 21:43:36 -0400

    feat(vim): use solarized-flood theme for airline

--- users/matt/home.nix
+++ users/matt/home.nix
@@ -297,7 +297,7 @@ in
         vim-airline
         { plugin = vim-airline-themes;
           config = ''
-            let g:airline_theme='solarized'
+            let g:airline_theme='solarized_flood'
             let g:airline_solarized_bg='dark'
             let g:airline_powerline_fonts = 1
 
