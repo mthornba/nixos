6d1b0f5c1d011c8dc6db87e7ba1d3330ab513e3d
Matt Thornback <matt.thornback@palitronica.com>
2025-06-16 16:51:48 -0400

    feat(vim): configure toggleterm

--- users/matt/modules/neovim.nix
+++ users/matt/modules/neovim.nix
@@ -107,14 +107,14 @@
         shade_filetypes = {},
         autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
         shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
-        shading_factor = '-30', -- the percentage by which to lighten dark terminal background, default: -30
+        shading_factor = '-15', -- the percentage by which to lighten dark terminal background, default: -30
         shading_ratio = '-3', -- the ratio of shading factor for light/dark terminal background, default: -3
         start_in_insert = true,
         insert_mappings = true, -- whether or not the open mapping applies in insert mode
         terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
         persist_size = true,
         persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
-        direction = 'horizontal',
+        direction = 'tab',
         close_on_exit = true, -- close the terminal window when the process exits
         clear_env = false, -- use only environmental variables from `env`, passed to jobstart()
         auto_scroll = true, -- automatically scroll to the bottom on terminal output
@@ -132,7 +132,7 @@
           -- title_pos = 'left' | 'center' | 'right', position of the title of the floating window
         },
         winbar = {
-          enabled = false,
+          enabled = true,
           name_formatter = function(term) --  term: Terminal
             return term.name
           end
