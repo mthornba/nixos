1484483af841b87dab6fde58d8c9042c05e25dba
Matt Thornback <matt.thornback@palitronica.com>
2025-06-15 22:28:46 -0400

    feat(vim): add toggleterm

--- users/matt/home.nix
+++ users/matt/home.nix
@@ -289,6 +289,64 @@ in
         --     },
         --   },
         -- }),
+
+        return require("toggleterm").setup{
+          -- size can be a number or function which is passed the current terminal
+          function(term)
+            if term.direction == "horizontal" then
+              return 15
+            elseif term.direction == "vertical" then
+              return vim.o.columns * 0.4
+            end
+          end,
+          open_mapping = [[<c-t>]],
+          -- on_create = fun(t: Terminal), -- function to run when the terminal is first created
+          -- on_open = fun(t: Terminal), -- function to run when the terminal opens
+          -- on_close = fun(t: Terminal), -- function to run when the terminal closes
+          -- on_stdout = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stdout
+          -- on_stderr = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stderr
+          -- on_exit = fun(t: Terminal, job: number, exit_code: number, name: string) -- function to run when terminal process exits
+          hide_numbers = true, -- hide the number column in toggleterm buffers
+          shade_filetypes = {},
+          autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
+          shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
+          shading_factor = '-30', -- the percentage by which to lighten dark terminal background, default: -30
+          shading_ratio = '-3', -- the ratio of shading factor for light/dark terminal background, default: -3
+          start_in_insert = true,
+          insert_mappings = true, -- whether or not the open mapping applies in insert mode
+          terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+          persist_size = true,
+          persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
+          direction = 'horizontal',
+          close_on_exit = true, -- close the terminal window when the process exits
+          clear_env = false, -- use only environmental variables from `env`, passed to jobstart()
+          auto_scroll = true, -- automatically scroll to the bottom on terminal output
+          float_opts = {
+            -- The border key is *almost* the same as 'nvim_open_win'
+            -- see :h nvim_open_win for details on borders however
+            -- the 'curved' border is a custom border type
+            -- not natively supported but implemented in this plugin.
+            border = 'double',
+            -- like `size`, width, height, row, and col can be a number or function which is passed the current terminal
+            -- width = <value>,
+            -- height = <value>,
+            -- row = <value>,
+            -- col = <value>,
+            -- title_pos = 'left' | 'center' | 'right', position of the title of the floating window
+          },
+          winbar = {
+            enabled = false,
+            name_formatter = function(term) --  term: Terminal
+              return term.name
+            end
+          },
+          responsiveness = {
+            -- breakpoint in terms of `vim.o.columns` at which terminals will start to stack on top of each other
+            -- instead of next to each other
+            -- default = 0 which means the feature is turned off
+            horizontal_breakpoint = 135,
+          }
+        },
       '';
 
       plugins = with pkgs.vimPlugins; [
@@ -313,6 +371,33 @@ in
             nnoremap <leader>fg <cmd>Telescope live_grep<cr>
             nnoremap <leader>fb <cmd>Telescope buffers<cr>
             nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+          '';
+        }
+        { plugin = toggleterm-nvim;
+          config = ''
+            " set
+            autocmd TermEnter term://*toggleterm#*
+                  \ tnoremap <silent><C-t> <Cmd>exe v:count1 . "ToggleTerm"<CR>
+
+            " By applying the mappings this way you can pass a count to your
+            " mapping to open a specific window.
+            " For example: 2<C-t> will open terminal 2
+            nnoremap <silent><C-t> <Cmd>exe v:count1 . "ToggleTerm"<CR>
+            inoremap <silent><C-t> <Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>
+
+            "function _G.set_terminal_keymaps()
+            "  local opts = {buffer = 0}
+            "  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+            "  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+            "  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+            "  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+            "  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+            "  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+            "  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+            "end
+            "
+            "-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+            "vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
           '';
         }
         vim-airline
