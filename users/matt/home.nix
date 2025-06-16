8f76d186624368041f6c3f60631b19d59e4ffec4
Matt Thornback <matt.thornback@palitronica.com>
2025-06-15 22:26:24 -0400

    fix(vim): comment out oatmeal plugin

--- users/matt/home.nix
+++ users/matt/home.nix
@@ -278,18 +278,19 @@ in
         set smartindent
         " Soft tabstop (optional, but often helpful)
         set softtabstop=2
-        '';
-#      extraLuaConfig = ''
-#        return require('lazy').setup({
-#          {
-#            "dustinblackman/oatmeal.nvim",
-#            cmd = { "Oatmeal" },
-#            keys = {
-#                { "<leader>om", mode = "n", desc = "Start Oatmeal session" },
-#            },
-#          },
-#        })
-#      '';
+      '';
+      extraLuaConfig = ''
+        -- require('lazy').setup({
+        --   {
+        --     "dustinblackman/oatmeal.nvim",
+        --     cmd = { "Oatmeal" },
+        --     keys = {
+        --         { "<leader>om", mode = "n", desc = "Start Oatmeal session" },
+        --     },
+        --   },
+        -- }),
+      '';
+
       plugins = with pkgs.vimPlugins; [
         ale
         git-blame-nvim
