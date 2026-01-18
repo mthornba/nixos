60f8cbf294a646b9261f4132c720cf2bfb7f622d
Matt Thornback <matt.thornback@palitronica.com>
2026-01-18 16:35:51 -0500

    feat(vim): use tab for autocompletion navigation

--- users/matt/modules/neovim.nix
+++ users/matt/modules/neovim.nix
@@ -153,15 +153,29 @@
       })
       vim.lsp.enable('terraformls')
       
-      require('cmp').setup({
+      local cmp = require('cmp')
+      cmp.setup({
         sources = {
           { name = 'nvim_lsp' },
           { name = 'path' },
           { name = 'buffer' },
         },
-        mapping = require('cmp').mapping.preset.insert({
-          ['<C-Space>'] = require('cmp').mapping.complete(),
-          ['<CR>'] = require('cmp').mapping.confirm({ select = true }),
+        mapping = cmp.mapping.preset.insert({
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
         }),
       })
 
