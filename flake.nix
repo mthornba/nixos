1578385de8ceed4a5eeda6759c80b5134e026a9a
Matt Thornback <matt.thornback@palitronica.com>
2026-05-28 09:52:52 -0400

    feat(nix): update flake pkg sources to 26.05

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "neon System Config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.11";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-26.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
     homebrew-bundle = { url = "github:homebrew/homebrew-bundle"; flake = false; };
